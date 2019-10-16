a2a243d74246645047fd01dc5504211d711c5766
Alex Shearer <alex.shearer23@gmail.com>
2019-10-16 14:41:24 -0500

    Added styling and template weather card

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
 (function () {
-    getWeatherInfo();
-    geocode();
+    //Submit button event handler
+    $("#submit").click(function(){
+        //Get the value the user has entered in the search bar and store it
+        const searchLocation = $("#searchBar").val();
+        //Call the geocode function and pass in the value
+        geocode(searchLocation);
+        //Clear out the search bar
+        $("#searchBar").val("");
+    })
 })();
 
-function getWeatherInfo() {
+function getWeatherInfo(ladtitude, longitude, city, state) {
 
     $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/" + "37.8267,-122.4233", { dataType: "jsonp"})
 
     .done(function(data){
         console.log(data);
+        let locations = data.results[0].locations[0];
+
+        let lat = locations.latLng.lat;
+        let lng = locations.latLng.lng;
+
+        let city = locations.adminArea5;
+        let state = locations.adminArea3;
+
+        console.log(lat);
+        console.log(lng);
+
+        console.log(city);
+        console.log(state);
+
+        getWeatherInfo(lat, lng, city, state)
     })
     .fail(function(error){
         console.log(error);
@@ -19,8 +41,8 @@ function getWeatherInfo() {
 
 }
 
-function geocode() {
-    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=Washington,DC")
+function geocode(location) {
+    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=" + location)
     .done(function(data){
         console.log(data);
     })
@@ -31,3 +53,10 @@ function geocode() {
         console.log("Geociding call complete");
     })
 }
+
+(function () {
+    $("#submit").click(function() {
+        console.log("Submit button clicked");
+    }) 
+})();
+
